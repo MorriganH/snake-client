a0ce17961080be0b723f1227f043df010e38cef6
MorriganH <morriganhennessy@gmail.com>
2023-04-12 14:24:29 -0400

    adds functions to aquire inputs and handle inputs

--- play.js
+++ play.js
@@ -1,1 +1,20 @@
+const { stdin } = require('process');
 const { connect } = require('./client');
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin.on('data', handleUserInput);
+
+setupInput();
